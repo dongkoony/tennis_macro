d23e07db2b7c66cfb34a19cbc7fa35a760c88e47
donghyeon shin <109497684+dongkoony@users.noreply.github.com>
2023-09-21 17:46:06 +0900

    Auto_Login & Auto_Local add

--- test.py
+++ test.py
@@ -9,21 +9,19 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Test_Macro'
-        self.settings = QSettings('MyCompany', 'MyApp')  # For saving and retrieving user credentials
+        self.settings = QSettings('MyCompany', 'MyApp')
         self.initUI()
-        
+
     def initUI(self):
         self.setWindowTitle(self.title)
         layout = QVBoxLayout()
 
-        # User ID input
         layout.addWidget(QLabel('아이디'))
         self.username_input = QLineEdit(self)
         self.username_input.setPlaceholderText('아이디 입력')
         self.username_input.setText(self.settings.value("username", ""))
         layout.addWidget(self.username_input)
 
-        # Password input
         layout.addWidget(QLabel('비밀번호'))
         self.password_input = QLineEdit(self)
         self.password_input.setPlaceholderText('비밀번호 입력')
@@ -31,7 +29,11 @@ class App(QWidget):
         self.password_input.setText(self.settings.value("password", ""))
         layout.addWidget(self.password_input)
 
-        # Checkbox to remember the user credentials
+        layout.addWidget(QLabel('원하는 지역'))
+        self.location_input = QLineEdit(self)
+        self.location_input.setPlaceholderText('지역 이름 입력')
+        layout.addWidget(self.location_input)
+
         self.remember_credentials_checkbox = QCheckBox('아이디/비밀번호 저장', self)
         self.remember_credentials_checkbox.setChecked(self.settings.value("rememberCredentials", False, type=bool))
         layout.addWidget(self.remember_credentials_checkbox)
@@ -58,6 +60,7 @@ class App(QWidget):
     def run_automation(self):
         username = self.username_input.text()
         password = self.password_input.text()
+        desired_location = self.location_input.text()  # 원하는 지역 정보를 가져옵니다.
 
         if self.remember_credentials_checkbox.isChecked():
             self.settings.setValue("username", username)
@@ -68,27 +71,27 @@ class App(QWidget):
             self.settings.remove("password")
             self.settings.setValue("rememberCredentials", False)
 
-        date = self.date_label.text()
-
-        # Selenium 웹 드라이버 초기화
         driver = webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
+        driver.maximize_window()
         driver.get("https://yeyak.seoul.go.kr/web/search/selectPageListDetailSearchImg.do?code=T100&dCode=T108")
         
         try:
-            # 아이디와 비밀번호 입력 필드 찾기
-            username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "actual_username_field_id_here")))
-            password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "actual_password_field_id_here")))
-
-            # 아이디와 비밀번호 입력
-            username_field.send_keys(username)
-            password_field.send_keys(password)
-
             # 로그인 버튼 클릭
-            login_button = driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div/div[1]/a')
-            login_button.click()
+            login_page_button = driver.find_element(By.XPATH, '//*[@id="header"]/div[1]/div/div[1]/a')
+            login_page_button.click()
 
-            print(f"Automating with {username}, {password}, {date}")
-        
+            # 아이디, 패스워드 기입 및 로그인
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="userid"]'))).send_keys(username)
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="userpwd"]'))).send_keys(password)
+            driver.find_element(By.XPATH, '//*[@id="addUserForm"]/div[1]/button').click()
+
+            # 원하는 지역 검색
+            search_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="search2"]/fieldset/div[2]/input')))
+            search_field.send_keys(desired_location)  # 지역 정보를 기입합니다.
+            driver.find_element(By.XPATH, '//*[@id="search2"]/fieldset/button[1]').click()
+
+            print(f"Automating with {username}, {password}")
+
         except Exception as e:
             print(f"An error occurred: {e}")
 
